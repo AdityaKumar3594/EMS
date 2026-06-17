5ba92fc7b5dad46b892b26522b8da2c193ce0c74
Aditya Kumar <camp25674@gmail.com>
2026-06-17 11:29:23 +0530

    Login Page created

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,12 +1,58 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>
-        
-      
-    </div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  //Two-way Binding
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    console.log("Email:", email);
+    console.log("Password:", password);
+
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      
+      <div className="border-2 rounded-xl border-emerald-600 p-20">
+        
+        <form
+          onSubmit={submitHandler}
+          className="flex flex-col items-center justify-center"
+        >
+          <label className="outline-none bg-transparent border-2 text-emerald-600 font-medium text-lg py-2 px-6 placeholder:text-gray-400 mb-10">EMS</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            type="email"
+            placeholder="Enter your email"
+            className=" text-white outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full placeholder:text-gray-400"
+          />
+
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            type="password"
+            placeholder="Enter password"
+            className="text-white outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full mt-3 placeholder:text-gray-400"
+          />
+
+          <button
+            type="submit"
+            className="mt-7 text-white hover:bg-emerald-700 font-semibold bg-emerald-600 text-lg py-2 px-8 w-full rounded-full"
+          >
+            Log in
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
